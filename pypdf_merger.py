56c1d91b4be0420405d63764c76c96cf1f520164
soushou1120 <gamesoushou1120@gmail.com>
2023-08-29 21:08:50 +0900

    ndl contents

---
+++ pypdf_merger.py
@@ -0,0 +1,81 @@
+import pypdf
+import os
+import re
+
+
+# マージするPDFファイルが入っているフォルダを指定
+folder_path = 'D://s_dl/ndl/src'
+## フォルダにPDFファイルが入っていない場合は終了
+## 入っている場合は"マージ作業を開始します"と表示
+if not os.listdir(folder_path):
+    print('フォルダにPDFファイルが入っていません')
+    exit()
+else:
+    print('マージ作業を開始します')
+
+# PDFファイルの名前とパスを取得
+pdf_files = []
+for filename in os.listdir(folder_path):
+    if filename.endswith('.pdf'):
+        pdf_files.append(os.path.join(folder_path, filename))
+## PDFファイルの数を表示
+print('PDFファイルの数: ' + str(len(pdf_files)))
+
+# ndlの永続的識別子
+ndl_id = r"digidepo_(\d+)_"
+
+# ndl_idごとにファイルをグループ化
+pdf_files_grouped = {}
+for pdf_file in pdf_files:
+    ndl_id_match = re.search(ndl_id, pdf_file)
+    if ndl_id_match:
+        extracted_ndl_id = ndl_id_match.group(1)
+        if extracted_ndl_id in pdf_files_grouped:
+            pdf_files_grouped[extracted_ndl_id].append(pdf_file)
+        else:
+            pdf_files_grouped[extracted_ndl_id] = [pdf_file]
+## PDFグループの数を表示
+print('PDFグループの数: ' + str(len(pdf_files_grouped)))
+
+# ndl_idごとにPDFファイルをソートしてマージ
+for ndl_id, pdf_files in pdf_files_grouped.items():
+    ## 何個目のグループを処理しているか表示
+    print('PDFグループ' + str(list(pdf_files_grouped.keys()).index(ndl_id) + 1) + 'を処理中')
+    
+    ## PDFファイルのリストをソート
+    ## 括弧がない場合は、括弧がある場合よりも前に来るようにする
+    ## 括弧がある場合は、括弧の中身でソート
+    def sort_key(pdf_file):
+        match = re.search(r"\((\d+)\)", pdf_file)
+        if match:
+            return int(match.group(1))
+        else:
+            return -1
+    pdf_files.sort(key=sort_key)
+    
+    ## マージしたPDFファイルの名前を設定
+    merged_file_name = 'digidepo_' + ndl_id + '_merged.pdf'
+    merger = pypdf.PdfMerger()
+    for pdf_file in pdf_files:
+        merger.append(pdf_file)
+
+    ## メタデータを設定
+    d = pypdf.PdfReader(pdf_files[0]).metadata
+    d = {k: d[k] for k in d.keys()}
+    merger.add_metadata(d)
+
+    ## メタデータの内、Keywrodsを使って、フォルダ名を設定
+    folder_name = d['/Keywords'].split(',')[0]
+    ## folder_nameからフォルダー名に使えない文字を削除
+    folder_name = re.sub(r'[\\/:*?"<>|]', '', folder_name)
+    folder_name = ndl_id + '_' + folder_name
+
+    ## マージしたPDFファイルを出力
+    ## フォルダを作成して、その中に出力
+    if not os.path.exists("D://s_dl/ndl/lib/" + folder_name):
+        os.mkdir("D://s_dl/ndl/lib/" + folder_name)
+    merger.write("D://s_dl/ndl/lib/" + folder_name + '/' + merged_file_name)
+    merger.close()
+    print('PDFグループ' + str(list(pdf_files_grouped.keys()).index(ndl_id) + 1) + 'を処理しました')
+
+print('マージ作業が完了しました')
